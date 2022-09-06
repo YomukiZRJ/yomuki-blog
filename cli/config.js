571fe19a3c5b85ad7fd15454f4cc4b55a5df66f2
zengrujing <yomuki@qq.com>
2022-09-06 13:51:23 +0800

    build: 🛠 cli

---
+++ cli/config.js
@@ -0,0 +1,27 @@
+export const LIST_MAP = {
+  bundlers: {
+    title: "构建",
+    children: {
+      vite: {
+        title: "vite",
+        folder: "bundlers/vite",
+      },
+      webpack: {
+        title: "webpack",
+        folder: "bundlers/webpack",
+      },
+      common: {
+        title: "通用",
+        folder: "bundlers",
+      },
+    },
+  },
+  "informal-essay": {
+    title: "随笔",
+    folder: "informal-essay",
+  },
+  "quick-check": {
+    title: "速查",
+    folder: "quick-check",
+  },
+};
